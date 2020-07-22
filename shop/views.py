f11b0de5d7eb1122ac75376a83874c496eb00f22
JinHee <327jhl@naver.com>
2020-07-22 22:27:14 +0900

    13 모델을 통한 데이터 조회

--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 def archives_year(request,year):
     return HttpResponse('{}년도에 대한 내용'.format(year))
+
+def item_list(request):
+    qs = Item.objects.all()
+    q = request.GET.get('q','')
+    if q:
+        qs = qs.filter(name__icontains=q)
+
+    return render(request,'shop/item_list.html',{
+        'item_list':qs,
+        'q':q,
+    })
