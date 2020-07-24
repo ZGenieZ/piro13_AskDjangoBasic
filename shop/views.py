af9afd60a00e9aac64ac7bed99640bb36b2903f1
JinHee <327jhl@naver.com>
2020-07-24 16:49:04 +0900

    30 장고 Form을 통해, 글 생성/수정 구현하기

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404,render,redirect
+from django.views.generic import CreateView, UpdateView
 from django.http import HttpResponse
 from .models import Item
+from .forms import ItemForm
 
 # Create your views here.
 def archives_year(request,year):
@@ -22,3 +24,25 @@ def item_detail(request,pk):
     return render(request,'shop/item_detail.html',{
         'item': item,
     })
+
+# def item_new(request, item=None):
+#     if request.method == 'POST':
+#         form = ItemForm(request.POST, request.FILES,instance=item)
+#         if form.is_valid():
+#             item = form.save()  #ModelForm에서 제공
+#             return redirect(item)
+#     else:
+#         form = ItemForm(instance=item)
+
+#     return render(request, 'shop/item_form.html', {
+#         'form' : form,
+#     })
+
+
+# def item_edit(request, pk):
+#     item = get_object_or_404(Item, pk=pk)
+#     return item_new(request, item)
+
+# 위의 2개의 함수를 이렇게 간단하게 CreateView, UpdateView로 구현 가능
+item_new =CreateView.as_view(model=Item,form_class=ItemForm)
+item_edit =UpdateView.as_view(model=Item,form_class=ItemForm)
